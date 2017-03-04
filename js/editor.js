e7507e5bf4fc161a751048ae98a63883674dc27d
Sergey Shambir <sshambir@yandex.ru>
2017-03-04 15:58:57 +0300

    Добавил поддержку Copy на странице с кодом Markdown

--- js/editor.js
+++ js/editor.js
@@ -1,8 +1,25 @@
+/*var converter = new showdown.Converter();
+converter.setOption('ghCodeBlocks', false);
 
-function initEditor()
+var el = document.getElementById('editor');
+el.addEventListener ("paste", function(e) {
+    e.preventDefault();
+    var input = e.clipboardData.getData('Text');
+    var html = converter.makeHtml(input);
+    e.clipboardData.setData('Text', html);
+    document.execCommand("insertHTML", false, html);
+    console.log(html);
+}, false);*/
+
+function MarkdownEditor()
 {
-    var editorDiv = document.getElementById('editor');
-    var options = {
+    let editorDiv = document.getElementById('editor');
+    let overlayDiv = document.querySelector('.markdown.overlay');
+    let markdownText = document.querySelector('.markdown .popup .content');
+    let popupCloseDiv = document.querySelector('.markdown .popup .popup-close');
+    let copyStatusSpan = document.querySelector('.copy-clipboard .copy-status')
+
+    let options = {
         editor: editorDiv, // {DOM Element} [required]
         debug: true, // {Boolean} false by default
         stay: true, // Stay on page when unsafe page redirect happens
@@ -13,32 +30,44 @@ function initEditor()
             'insertimage': 'Image',
         }
     };
-    var editor = new Pen(options);
 
-    /*var converter = new showdown.Converter();
-    converter.setOption('ghCodeBlocks', false);
+    this.editor = new Pen(options);
+    this.markdownShown = false;
 
-    var el = document.getElementById('editor');
-    el.addEventListener ("paste", function(e) {
-        e.preventDefault();
-        var input = e.clipboardData.getData('Text');
-        var html = converter.makeHtml(input);
-        e.clipboardData.setData('Text', html);
-        document.execCommand("insertHTML", false, html);
-        console.log(html);
-    }, false);*/
-
-    let overlayDiv = document.querySelector('.markdown.overlay');
-    let markdownPre = document.querySelector('.markdown .popup .content');
-    let markdownButton = document.querySelector('button.get-markdown');
-
-    document.querySelector('button.get-markdown').addEventListener('click', function() {
-        markdownPre.innerHTML = editor.toMd();
+    this.showMarkdownCode = function() {
+        markdownText.value = this.editor.toMd();
+        copyStatusSpan.classList.remove('shown');
         overlayDiv.classList.add('overlay-on');
-    });
-    document.querySelector('.markdown .popup .popup-close').addEventListener('click', function() {
+        this.editor.destroy();
+        this.markdownShown = true;
+    }.bind(this);
+
+    this.copyMarkdownCode = function() {
+        if (this.markdownShown)
+        {
+            markdownText.select();
+            document.execCommand('copy');
+            copyStatusSpan.classList.add('shown');
+        }
+    }.bind(this);
+
+    this.hideMarkdownCode = function() {
+        this.markdownShown = false;
+        this.editor.rebuild();
         overlayDiv.classList.remove('overlay-on');
-    });
+    }.bind(this);
+
+    popupCloseDiv.addEventListener('click', this.hideMarkdownCode.bind(this));
+}
+
+
+function initEditor()
+{
+    let editor = new MarkdownEditor();
+    let markdownButton = document.querySelector('button.get-markdown');
+    let copyClipboardButton = document.querySelector('button.copy-clipboard');
+    markdownButton.addEventListener('click', editor.showMarkdownCode);
+    copyClipboardButton.addEventListener('click', editor.copyMarkdownCode);
 };
 
 document.addEventListener("DOMContentLoaded", initEditor);
